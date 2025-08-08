a9b256ea1d9d4c5a6553265a53343421b7d3c8fb
ShashankPT07 <shashankpt.xworks@gamil.com>
2025-08-08 16:22:54 +0530

    java line

---
+++ Bucket.java
@@ -0,0 +1,14 @@
+class Bucket{
+public static void price(){
+	System.out.println("100");
+}
+public static void quantity(){
+	price();
+System.out.println("43");
+}
+
+public static void length(){
+	quantity();
+	System.out.println("45.2");
+}
+}
